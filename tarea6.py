db20d49ceb7ac6a1f9486d53b23b36a80d3b04a8
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-10-23 12:42:34 +0200

    la tarea 6 había que intruducir un int

---
+++ tarea6.py
@@ -0,0 +1,8 @@
+print("introduce la edad que tienes")
+age = int(input())
+print (f"la edad introducida es: {age}")
+
+if age < 17:
+    print("you are NOT old enough to drive")
+else:
+    print("you can drive")
