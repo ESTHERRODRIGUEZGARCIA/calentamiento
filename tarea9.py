07a493556ed1faf84932ca19523fc81fbb6a193c
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-10-25 16:53:06 +0200

    tarea 9 (he borrad la primera parte)

--- tarea9.py
+++ tarea9.py
@@ -1,3 +1,7 @@
-f = open("file_name", "r")
-print(f.read())
-open()
+f = open("demofile1.txt", "a")
+f.write("The file will include more text...")
+f.close()
+
+f = open("demofile2.txt", "w")
+f.write("demofile2 file created, with this content in!")
+f.close()
