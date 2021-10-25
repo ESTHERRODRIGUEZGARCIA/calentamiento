1daa0dd8420db3e5bb84a47dff312c7e8b47b8ef
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-10-25 16:17:40 +0200

    parte 2 de la tarea 8 hecha

---
+++ tarea8b.py
@@ -0,0 +1,10 @@
+def calcCost(item):
+    if(item == "sweets"):
+        return 3.99
+    elif (item == "oranges"):
+        return 1.99
+    else:
+        return 0.99
+spent = 10
+spent = spent + calcCost("sweets")
+print("you have spent:" + str(spent))
