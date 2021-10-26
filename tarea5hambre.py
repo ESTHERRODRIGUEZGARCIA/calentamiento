5ddad9c26e3ff79909e6dfeef2a2276e3ec4803b
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-10-26 20:19:16 +0200

    tiene que haber algún fallo :((((((((

--- tarea5hambre.py
+++ tarea5hambre.py
@@ -1,7 +1,8 @@
 #dinero inicial, 2000. 
-#satisfecho: entre 85 y 100%
+#satisfecho: entre 85 y 100% -> True
 #precio inicial del helado, 100; cada vez que compres otro aumenta 20%
 #si tienes x años, tienes x% de hambre
 
 
-dinero_inicial = 2000
+dinero = 2000
+#algo no va
