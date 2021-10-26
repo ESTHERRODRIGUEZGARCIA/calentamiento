ed13dbb7370a73d90a7833967e5a91dd2d3a9175
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-10-26 17:40:36 +0200

    vuelta a empezar con el problema del hambre

--- tarea4altura.py
+++ tarea4altura.py
@@ -1,3 +1,4 @@
 altura = 200
 suma = 50
+print("la altura total es: ")
 print(altura + suma)
---
+++ tarea5hambre.py
@@ -0,0 +1,7 @@
+#dinero inicial, 2000. 
+#satisfecho: entre 85 y 100%
+#precio inicial del helado, 100; cada vez que compres otro aumenta 20%
+#si tienes x años, tienes x% de hambre
+
+
+dinero_inicial = 2000
