df6fe7feaa1cd6686e8dcfe179f536da676a46b2
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-10-27 17:48:28 +0200

    no se cuantos bitcoins tengo, supongo que uno

--- tarea8bitcoin.py
+++ tarea8bitcoin.py
@@ -1,4 +1,4 @@
-bitcoin_amount = 1.2 #cuanto tengo en bitcoin
+bitcoin_amount = 1 #cuanto tengo en bitcoin
 bitcoin_value_euros = 25000 #el valor de bitcoin en euros
 
 def bitcoinToEuros(bitcoin_amount, bitcoin_value_euros):
@@ -8,6 +8,6 @@ def bitcoinToEuros(bitcoin_amount, bitcoin_value_euros):
 investment_in_euros = bitcoinToEuros(bitcoin_amount, bitcoin_value_euros)
 
 if bitcoin_value_euros <= 30000:
-    print("investment below 30.000€! SELL!")
+    print("Investment below 30.000€! SELL!")
 else:
     print("Investment above 30.000€")
