1649aa827447d386112270ae5ca9300516a47a78
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-10-29 12:45:47 +0200

    la 5 la estoy entendiendo mal o no se hacerla

--- tarea5hambre.py
+++ tarea5hambre.py
@@ -5,4 +5,13 @@
 
 
 dinero = 2000
+hambre = 17
+if dinero >= 1700 and dinero <= 2000:
+    print ("no tengo hambre")
+precio_helado = 0.17 * 100
+
+def compro_helado():
+     dinero -= precio_helado
+     print("el precio del helado es, ", str(precio_helado))
+
 #algo no va
