69903b14c824dc7d3d20e3a796e5c286eec3447b
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-10-30 10:06:28 +0200

    el colorama sigue sin ir :(

--- tarea1.py
+++ tarea1.py
@@ -1,2 +1,4 @@
 print("Roses are #ff0000, Violets are #0000ff why my code´s working" + "I haven´t a clue")
-print("lo que quieras")
+import colorama
+from colorama import Fore, init colorama.init()
+print((Fore.red + "roses are red")+(Fore.blue + "violets are blue")+(Fore.white + "my code´s working"))
