ae3b42b8dc9dae7ad171ab7b98569753a8a0df0e
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-10-30 10:11:39 +0200

    tarea 1, supongo que e s fallo del ordenador que no va el colorama

--- tarea1.py
+++ tarea1.py
@@ -1,4 +1,4 @@
 print("Roses are #ff0000, Violets are #0000ff why my code´s working" + "I haven´t a clue")
 import colorama
-from colorama import Fore, init colorama.init()
-print((Fore.red + "roses are red")+(Fore.blue + "violets are blue")+(Fore.white + "my code´s working"))
+from colorama import Fore, init
+print((Fore.RED + "roses are red")+(Fore.BLUE + "violets are blue")+(Fore.WHITE + "my code´s working"))
